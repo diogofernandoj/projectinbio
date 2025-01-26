b39353de3c5772d73a4f07143d48254b135ea7d2
Diogo Jorge <diogofernandoj@gmail.com>
2025-01-26 10:33:16 -0300

    feat: add header and project card

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "./_components/lp/header";
 import Hero from "./_components/lp/hero";
 
 const Home = () => {
   return (
     <div className="max-w-7xl mx-auto">
+      <Header />
       <Hero />
     </div>
   );
