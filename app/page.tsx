a79c163c81b4fa33a3229156c10a349ca7066b47
Diogo Jorge <diogofernandoj@gmail.com>
2025-01-26 10:52:26 -0300

    feat: add faq

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import FAQ from "./_components/lp/faq";
 import Header from "./_components/lp/header";
 import Hero from "./_components/lp/hero";
+import Pricing from "./_components/lp/pricing";
+import VideoExplanation from "./_components/lp/video-explanation";
 
 const Home = () => {
   return (
     <div className="max-w-7xl mx-auto">
       <Header />
       <Hero />
+
+      <VideoExplanation />
+      <Pricing />
+      <FAQ />
     </div>
   );
 };
